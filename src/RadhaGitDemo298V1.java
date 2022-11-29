cabbcf5897bca9bd7eb0cbb628fdb2e2065cb485
balam <balam@LAPTOP-BR47I1JS>
2022-11-28 22:48:05 -0800

    testing second commit

--- src/RadhaGitDemo298V1.java
+++ src/RadhaGitDemo298V1.java
@@ -1,12 +1,12 @@
 
-public class Test1 {
+public class RadhaGitDemo298V1 {
 
 	public static void main(String[] args)
 	{
 		String hungry ="yes";
 		if(hungry == "yes") 
 		{
-			System.out.println("Eat something");
+			System.out.println("test push something");
 		}
 		else 
 		{
